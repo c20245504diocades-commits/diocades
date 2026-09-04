f483ffdf1de241052052131d40aff6a5eae28c6f
musubi <c20245504.diocades@csav.edu.ph>
2026-09-04 15:09:13 +0800

    variables (let and const)

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello world.");
+console.log("from script.js file.");
+
+const myName = "Jom";
+let age = 60;
+age = 18;
+const number = 91535667371;
+const address = "E B Magalona";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
+
